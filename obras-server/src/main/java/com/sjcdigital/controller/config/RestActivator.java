a2edf37eea399deef72a7465c783d70f9f6325f8
pedro-hos <pedro-hos@outlook.com>
2016-05-18 21:36:39 -0300

    Update 2016-05-19T00:36:26.923Z

--- obras-server/src/main/java/com/sjcdigital/controller/config/RestActivator.java
+++
@@ -1,7 +0,0 @@
-package com.sjcdigital.controller.config;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api/")
-public class RestActivator extends Application { }
--- obras-server/src/main/java/com/sjcdigital/model/repositories/impl/Cidades.java
+++
@@ -1,9 +0,0 @@
-package com.sjcdigital.model.repositories.impl;
-
-import javax.ejb.Stateless;
-
-import com.sjcdigital.model.entities.impl.Cidade;
-import com.sjcdigital.model.repositories.DefaultRepository;
-
-@Stateless
-public class Cidades extends DefaultRepository<Cidade> { }
